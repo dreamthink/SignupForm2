28a51fd66dbecbd1d68085a8ca480334dfebcf7e
Emily Chen <emily.c.chen@gmail.com>
2016-06-20 09:24:21 -0400

    Add second login form with highlighted error fields

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 angular.module("myApp", [])
-	.controller("myController", function() {
+	.controller("myController1", function() {
 		this.submitForm = function() {
-			alert("Username is " + this.username + "\nPassword is " + this.password);
+			alert("Form 1:\nUsername is " + this.username1 + "\nPassword is " + this.password1);
+		};
+	})
+	.controller("myController2", function() {
+		this.submitForm = function() {
+			alert("Form 2:\nUsername is " + this.username2 + "\nPassword is " + this.password2);
 		};
 	});
